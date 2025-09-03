8d79ab812dec39947177bb11581c6fa8f6a29687
cristian <cchura.cpc@gmail.com>
2025-09-02 21:08:18 -0400

    usuario sucursal migraciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { UsersModule } from './modules/admin/users/users.module';
 import { User } from './modules/admin/users/entities/user.entity';
 import { AuthModule } from './modules/auth/auth.module';
 import { InventarioModule } from './modules/admin/inventario/inventario.module';
+import { PermissionsModule } from './modules/admin/permissions/permissions.module';
+import { RolesModule } from './modules/admin/roles/roles.module';
 
 @Module({
   imports: [
@@ -24,7 +26,9 @@ import { InventarioModule } from './modules/admin/inventario/inventario.module';
     }),
     UsersModule,
     AuthModule,
-    InventarioModule
+    InventarioModule,
+    PermissionsModule,
+    RolesModule
   ],
   controllers: [AppController],
   providers: [AppService],
